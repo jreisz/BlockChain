5b085f9c4a4e39c40c59b70ead56020626915160
Juan Reisz <juan.reisz@techsquads.com>
2023-12-30 00:58:41 -0300

    try give support to deploy backend to render website

--- config/application.js
+++ config/application.js
@@ -5,12 +5,12 @@ const connectionOptions = {
     useFindAndModify:false,
     useUnifiedTopology: true
 }
-const port = 3456
+const port = 80
 const hostname = 'localhost'
-const uri = 'mongodb://localhost/ConcurrentQueue'
+//const uri = 'mongodb://127.0.0.1:27017'
+const uri = 'mongodb+srv://juanmanuelreisz:x4Ms6DN3pCCRzgYV@cluster0.5a9hnwa.mongodb.net/'
 const filePath ='public/csv/OutPut.csv'
 const difficulty = 2
-
 module.exports.difficulty = difficulty
 module.exports.port = port
 module.exports.hostname = hostname
